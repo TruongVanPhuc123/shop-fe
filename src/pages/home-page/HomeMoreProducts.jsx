98ff60f62f91e280e5c608444ef53b49ac16a66b
TVPhuc <tvphuc2k4@gmail.com>
2024-09-16 20:37:10 +0700

    final

--- src/pages/home-page/HomeMoreProducts.jsx
+++ src/pages/home-page/HomeMoreProducts.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 function HomeMoreProducts() {
   return (
     <section>
-      <div className="w-full h-auto mt-10 flex flex-col items-center gap-10  p-10">
-        <div className="grid grid-rows-2 2xl:grid-rows-1 grid-flow-col gap-4">
+      <div className="w-full h-auto mt-10 flex flex-col items-center gap-10 p-10">
+        <div className="grid grid-rows-2 xl:grid-rows-1 grid-flow-col gap-4">
           <CardProduct
             className={"flex flex-col items-center justify-between"}
             src={"/Shiny Happy - Plants 3.png"}
